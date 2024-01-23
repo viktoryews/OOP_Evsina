6bc57379670f6d3427e7f852571dc66e5f709863
Viktorya Evsina <viktoryews1997@mail.ru>
2024-01-23 20:57:16 +0300

    *

--- scrip.js
+++ scrip.js
@@ -1,21 +1,64 @@
 /** 1 */
-// Реализуйте класс Employee (сотрудник), который имеет следующие свойства и методы:
+class Employee {
+    constructor(name) {
+        this.name = name;
+    }
+    displayInfo() {
+        console.log(`${this.name}`);
+    }
+}
 
-// Свойство name (имя) - строка, имя сотрудника.
-// Метод displayInfo() - выводит информацию о сотруднике (имя).
-// Реализуйте класс Manager (менеджер), который наследует класс Employee и имеет дополнительное свойство и метод:
+class Manager extends Employee {
+    constructor(name, department) {
+        super(name);
+        this.department = department;
+    }
+    displayInfo() {
+        console.log(`${this.name} работает в отделе ${this.department}`);
+    }
+}
 
-// Свойство department (отдел) - строка, отдел, в котором работает менеджер.
-// Метод displayInfo() - переопределяет метод displayInfo() родительского класса и выводит информацию о менеджере (имя и отдел).
-// // Пример использования классов
-// const employee = new Employee(""John Smith"");
-// employee.displayInfo();
-// // Вывод:
-// // Name: John Smith
+const employee = new Employee('John Smith');
+employee.displayInfo();
 
-// const manager = new Manager(""Jane Doe"", ""Sales"");
-// manager.displayInfo();
-// // Вывод:
-// // Name: Jane Doe
-// // Department: Sales
+const manager = new Manager('Jane Doe', 'Sales');
+manager.displayInfo();
 
+/** 2 */
+class Order {
+    constructor(orderNumber) {
+        this.orderNumber = orderNumber;
+        this.arrProducts = [];
+        this.totalPrice = 0;
+    }
+
+    addProduct(product) {
+        this.arrProducts.push(product);
+    }
+
+    getTotalPrice() {
+        for (let value in this.arrProducts) {
+            this.totalPrice += this.arrProducts[value].price
+        }
+        return this.totalPrice;
+    }
+}
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+const order = new Order(12345);
+
+const product1 = new Product('Phone', 500);
+order.addProduct(product1);
+
+const product2 = new Product('Headphones', 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice());
+
+/** 3 */
